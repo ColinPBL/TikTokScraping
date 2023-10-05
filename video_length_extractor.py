c8890a79ffb232f2721f3d5458480143b26b7b7a
ColinPBL <colinpoubel@orange.fr>
2023-10-05 09:51:23 +0200

    Add script to extract durations from mp4 files

---
+++ video_length_extractor.py
@@ -0,0 +1,26 @@
+from os import chdir, listdir, path
+from io import open
+from tinytag import TinyTag
+import csv
+import re
+
+chdir("C:\\Users\\colin\\Documents\\Master RESO\\Memoire\\Data\\Echantillon\\Videos")
+
+video_directories = listdir("venv")
+
+with open("../videos_lengths.csv", "w", newline='') as output:
+
+    writer = csv.writer(output)
+    writer.writerow(["Account", "Video ID", "Video Length"])
+
+    for directory in video_directories:
+        if path.isdir(directory):
+            chdir("./" + directory)
+            files = listdir("venv")
+            for file in files:
+                if(path.isfile(file)):
+                    tag = TinyTag.get(file)
+                    account = re.search("(\D*)_(\d*)", file)
+                    print(f"Account : {account.group(1)}, Video ID : {account.group(2)}, Video Length : {tag.duration}")
+                    writer.writerow([account.group(1), account.group(2), tag.duration])
+            chdir("/")
