56485e516863905da34a1a0480144627358e52fa
ColinPBL <colinpoubel@orange.fr>
2023-10-17 11:53:06 +0200

    Add NMF to topic modelling pipeline
    TODO : use lemmatizer and POS instead of stemmer

--- topic_modelling.py
+++ topic_modelling.py
@@ -1,9 +1,12 @@
 import csv
 import re
+import matplotlib.pyplot as plt
 from os import chdir
 import nltk
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import NMF
+
 """
 Text processing requires several steps before analysis
 
@@ -12,8 +15,29 @@ a csv file with one document per line. A document can have several associated va
 in the processing step
 """
 
+
 def dummy_tokenizer(doc):
     return doc
+
+
+def plot_top_words(model, feature_names, n_top_words, title):
+    fig, axes = plt.subplots(3, 5, figsize=(30, 15), sharex=True)
+    axes = axes.flatten()
+    for topic_idx, topic in enumerate(model.components_):
+        top_features_ind = topic.argsort()[-n_top_words:]
+        top_features = feature_names[top_features_ind]
+        weights = topic[top_features_ind]
+
+        ax = axes[topic_idx]
+        ax.barh(top_features, weights, height=0.7)
+        ax.set_title(f"Topic {topic_idx + 1}", fontdict={"fontsize": 30})
+        ax.tick_params(axis="both", which="major", labelsize=20)
+        for i in "top right left".split():
+            ax.spines[i].set_visible(False)
+        fig.suptitle(title, fontsize=40)
+
+    plt.subplots_adjust(top=0.90, bottom=0.05, wspace=0.90, hspace=0.3)
+    plt.show()
 
 
 # Data cleaning
@@ -38,7 +62,7 @@ if debug:
     print(documents)
 # Tokenize
 # Only download on first run
-#nltk.download("punkt")
+# nltk.download("punkt")
 documents_list = documents["Transcript"]
 if debug:
     print("Only text values")
@@ -89,9 +113,23 @@ if debug:
     print(stems)
 
 # Compute the tf-idf matrix from stems
-transformer = TfidfVectorizer(tokenizer=dummy_tokenizer, preprocessor=dummy_tokenizer)
+transformer = TfidfVectorizer(tokenizer=dummy_tokenizer, preprocessor=dummy_tokenizer, max_df=0.5)
 
 tf_idf_matrix = transformer.fit_transform(stems)
+tfidf_feature_names = transformer.get_feature_names_out()
 
 if debug:
     print(tf_idf_matrix)
+    print(transformer.vocabulary_)
+
+# Topic extraction using NMF
+
+# Define a number of topics
+topics_number = 15
+
+# Create the nmf model
+nmf = NMF(n_components=topics_number, max_iter=500)
+
+nmf.fit(tf_idf_matrix)
+
+plot_top_words(nmf, tfidf_feature_names, 10, "Topics in NMF model (Frobenius norm)")
