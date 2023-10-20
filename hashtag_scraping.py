72c032d0901e8d80c3b9acdbbf84fed24a419eba
ColinPBL <colinpoubel@orange.fr>
2023-10-20 14:49:23 +0200

    Add bare minimum hashtag scraping script to be expanded upon later

--- hashtag_scraping.py
+++ hashtag_scraping.py
@@ -1,8 +1,12 @@
 from tiktokapipy.api import TikTokAPI
 
 def scrape_challenges(tag):
+    # Create an instance of the TikTokAPI class
     with TikTokAPI() as api:
+        # Get trending videos with the corresponding challenge, up to video_limit
         challenge = api.challenge(tag, video_limit=10)
+
+        # Print information about each video
         for video in challenge.videos:
             print("====================")
             print("Video desc : " + video.desc)
