64cb57de127e0d22c7e9b4e635b612fd170d8f5e
ColinPBL <colinpoubel@orange.fr>
2023-10-26 16:18:29 +0200

    Add playwright install script

--- hashtag_scraping.py
+++ hashtag_scraping.py
@@ -1,10 +1,11 @@
 from tiktokapipy.api import TikTokAPI
+import os
 
 def scrape_challenges(tag):
     # Create an instance of the TikTokAPI class
     with TikTokAPI() as api:
         # Get trending videos with the corresponding challenge, up to video_limit
-        challenge = api.challenge(tag, video_limit=10)
+        challenge = api.challenge(tag, video_limit=100)
 
         # Print information about each video
         for video in challenge.videos:
@@ -12,7 +13,8 @@ def scrape_challenges(tag):
             print("Video desc : " + video.desc)
             print("Video author : " + video.author.unique_id)
             print("Video creation time : " + str(video.create_time))
+            print("Video challenges : " + str(video.challenges))
 
 
-
-scrape_challenges("politique")
+os.system("playwright install")
+scrape_challenges("nutrition")
